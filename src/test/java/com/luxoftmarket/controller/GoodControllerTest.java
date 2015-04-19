7eb4061be251b480afc0fb90f2f4a1db03499f8e
Vladyslav Kostiuchenko <sadkoua@gmail.com>
2015-04-19 18:03:39 +0400

    1st unit test.

---
+++ src/test/java/com/luxoftmarket/controller/GoodControllerTest.java
@@ -0,0 +1,28 @@
+package com.luxoftmarket.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GoodControllerTest {
+
+    @Test
+    public void testGetBooks() throws Exception {
+        System.out.println("first test");
+    }
+
+    @Test
+    public void testAddGood() throws Exception {
+
+    }
+
+    @Test
+    public void testAddGood1() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteGood() throws Exception {
+
+    }
+}
