5a12b7b7a8e54f2d97c1a3151988ea1c9efe47d8
Vladyslav Kostiuchenko <sadkoua@gmail.com>
2015-05-01 14:55:11 +0400

    Testing.

--- src/test/java/com/luxoftmarket/dao/impl/GoodDaoImplTest.java
+++ src/test/java/com/luxoftmarket/dao/impl/GoodDaoImplTest.java
@@ -1,21 +1,25 @@
 package com.luxoftmarket.dao.impl;
 
 import com.luxoftmarket.domain.Good;
-import junit.framework.TestCase;
 import org.hibernate.Criteria;
-import org.hibernate.Session;
+//import org.hibernate.Session;
+import org.hibernate.Query;
+import org.hibernate.classic.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
@@ -27,78 +31,120 @@ import static org.mockito.Mockito.*;
 public class GoodDaoImplTest {
 
     @Mock
-//@Autowired
+@Autowired
     private SessionFactory sessionFactory;
 
 
     @InjectMocks
     GoodDaoImpl s;
 
-    //        public void add(Good good) { sessionFactory.getCurrentSession().save(good); }
-    @Test
+
+    @Test//(expected = Exception.class)
     public void testAdd() throws Exception {
+/*public void add(Good good) {sessionFactory.getCurrentSession().save(good); } */
+        final Good testGood = new Good();
+//        Session mockSession = mock(Session.class, withSettings().serializable());
+        Session mockSession = mock(Session.class, withSettings().serializable());
 
+        doReturn(mockSession).when(sessionFactory).getCurrentSession();
+        doAnswer(new Answer<Object>() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                if (args[0].equals(testGood)) {
+                    return "testGood";
+                } else {
+                    return "else";
+                }
+            }
+        }).when(mockSession).save(any(Good.class));
 
-//        SessionFactory sessionFactory = mock(SessionFactory.class);
-
-//        SessionFactory ses = mock(SessionFactory.class);
-//        Session ses = mock(sessionFactory.getCurrentSession().getClass());
-
-//        doNothing().when(ses).save(any(Good.class));
-        doNothing().when(sessionFactory.getCurrentSession()).save(any(Good.class));
-//        doNothing().when(goodDao).add(any(Good.class));
-
-//        Serializable serializable = null;
-//        doReturn(serializable).when(sessionFactory).getCurrentSession().save(any(Good.class));
-//        when(ses.get(anyInt())).thenReturn(null);
-
-        Good testGood = new Good();
         s.add(testGood);
 
-        verify(sessionFactory.getCurrentSession()).save(same(testGood));
+        verify(sessionFactory).getCurrentSession();
+        verify(mockSession).save(eq(testGood));
+
+        assertEquals("testGood", mockSession.save(testGood)); // Serializable
     }
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void testEdit() throws Exception {
 //        public void edit(Good good) { sessionFactory.getCurrentSession().update(good);}
+        /*//        public void edit(Good good) {
+         sessionFactory.getCurrentSession().update(good);}
+          */
+        final Good testGood = new Good();
+        Session mockSession = mock(Session.class);
+        doReturn(mockSession).when(sessionFactory).getCurrentSession();
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                    throw new RuntimeException();
+            }
+        }).when(mockSession).update(any(Good.class));
+//        doThrow(new Exception()).when(sessionFactory).getCurrentSession().update(any(Session.class));
+//        doNothing().when(sessionFactory).getCurrentSession().update(any(Good.class));
 
-
-        doNothing().when(sessionFactory).getCurrentSession().update(any(Good.class));
-
-        Good testGood = new Good();
+//        Good testGood = new Good();
         s.edit(testGood);
 
         verify(sessionFactory).getCurrentSession().update(same(testGood));
+//        assertEquals("testGood", mockSession.update(testGood)); //void type not allowed here
+//        assertEquals("testGood", mockSession.update(testGood));
     }
+
+
 
 
     /*        public void delete(int id) {
                 Good delGood = getGood(id);
                 sessionFactory.getCurrentSession().delete(delGood);}
  */
-    @Test
+    @Test//(expected = Exception.class)
     public void testDelete() throws Exception {
 
+/*        public void delete(int id) {
+Session tempSession = sessionFactory.getCurrentSession();
+Good tempGood = getGood(id);
+tempSession.delete(getGood(id));
+ */
+        GoodDaoImpl ses = mock(GoodDaoImpl.class);
+        Good mockGood = mock(Good.class);
         Good testGood = new Good();
-        //doReturn(testGood).when(s).getGood(5);
-        doNothing().when(sessionFactory).getCurrentSession().delete(Good.class);
+        Session mockSession = mock(Session.class);
+        doReturn(mockSession).when(sessionFactory).getCurrentSession();
+//        doReturn(testGood).when(ses).getGood(anyInt());
+        doNothing().when(mockSession).delete(testGood);
+
+//        doThrow(new Exception()).when(sessionFactory).getCurrentSession().delete(any(Good.class));
 
         s.delete(5);
 
-        verify(s).getGood(eq(5));
-        verify(sessionFactory).getCurrentSession().delete(eq(5));
+//        verify(s).getGood(eq(5));
+        verify(sessionFactory, times(2)).getCurrentSession();
+//        verify(s).getGood(eq(5));
+//        verify(mockSession).delete(testGood);
+//        verify(sessionFactory).getCurrentSession().delete(eq(5));
 
     }
 
-    //        public Good getGood(int id) { return (Good)sessionFactory.getCurrentSession().get(Good.class, id); }
+    /*        public Good getGood(int id) {
+                    Session session = sessionFactory.getCurrentSession();
+                    return (Good) session.get(Good.class, id);
+     }
+
+     */
     @Test
     public void testGetGood() throws Exception {
 
         Good toBeReturned = new Good();
-        doReturn(toBeReturned).when(sessionFactory).getCurrentSession().get(Good.class, anyInt());
+        Session mockSession = mock(Session.class);
+        doReturn(mockSession).when(sessionFactory).getCurrentSession();
+        doReturn(toBeReturned).when(mockSession).get(same(Good.class), anyInt());
 
         Good testGood = s.getGood(5);
-        verify(sessionFactory).getCurrentSession().get(Good.class, eq(5));
+
+        verify(sessionFactory).getCurrentSession();
+        verify(mockSession).get(Good.class, 5);
 
         assertSame(toBeReturned, testGood);
     }
@@ -108,38 +154,73 @@ public class GoodDaoImplTest {
             return sessionFactory.getCurrentSession().createQuery("from Good").list(); } */
     @Test
     public void testGetAllGood() throws Exception {
-
-
+/*
+    public List getAllGood() {
+     Session tempSession = sessionFactory.getCurrentSession();
+     Query tempQuery = tempSession.createQuery("from Good");
+     List returnedList = tempQuery.list();
+        return returnedList;
+    }
+ */
 
         List toBeReturned = Arrays.asList(new Good());
-        doReturn(toBeReturned).when(sessionFactory).getCurrentSession().createQuery("from Good").list();
+        Session mockSession = mock(Session.class);
+        Query mockQuery = mock(Query.class);
+
+        doReturn(mockSession).when(sessionFactory).getCurrentSession();
+        doReturn(mockQuery).when(mockSession).createQuery("from Good");
+        doReturn(toBeReturned).when(mockQuery).list();
 
         List testList = s.getAllGood();
 
-        verify(sessionFactory).getCurrentSession().createQuery("from Good").list();
+        verify(sessionFactory).getCurrentSession();
+        verify(mockSession).createQuery("from Good");
+        verify(mockQuery).list();
+
         assertSame(toBeReturned, testList);
     }
 
     @Test
     public void testFindGoodByName() throws Exception {
 /*        public Good findGoodByName(String goodname) {
-            Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Good.class);
+            Session tempSession = sessionFactrory.getCurrentSession();
+            Criteria criteria = tempSession.createCriteria(Good.class);
             criteria.add(Restrictions.eq("name", goodname));
             return (Good) criteria.uniqueResult();
         }*/
         Good toBeReturned = new Good();
-        Criteria testCriteria = mock(Criteria.class);
-        Criterion testCriterion = mock(Criterion.class);
+        Session mockSession = mock(Session.class);
+        Criteria mockCriteria = mock(Criteria.class);
+//        SimpleExpression mockSimpleExpression = mock(SimpleExpression.class);
+//        Restrictions mockRestrictions = mock(Restrictions.class);
+//        Restrictions mockRestrictions = mock(Restrictions.class);
 
-        doReturn(testCriteria).when(sessionFactory).getCurrentSession().createCriteria(Good.class);
-        doReturn(toBeReturned).when(testCriteria).uniqueResult();
+        doReturn(mockSession).when(sessionFactory).getCurrentSession();
+        doReturn(mockCriteria).when(mockSession).createCriteria(Good.class);
+        doReturn(toBeReturned).when(mockCriteria).uniqueResult();
+//        doNothing().when(mockRestrictions).eq(eq("name"), anyString()).thenReturn(mockCriteria);
+//        when(mockRestrictions.eq(anyString(), anyString())).thenReturn(mockSimpleExpression);
 
         Good testGood = s.findGoodByName("bobrik");
 
+        verify(sessionFactory).getCurrentSession();
+        verify(mockSession).createCriteria(eq(Good.class));
+        verify(mockCriteria).add(Restrictions.eq("name", "bobrik"));
 
-        verify(sessionFactory).getCurrentSession().createCriteria(Good.class);
-        verify(testCriteria).add(testCriterion);
-        verify(testCriteria).uniqueResult();
+/*      ВСЕ ЗАЕБЛО, ЗАЕБЛО, ЗАЕБЛО !!!
+
+Argument(s) are different! Wanted:
+criteria.add(name=bobrik);
+-> at com.luxoftmarket.dao.impl.GoodDaoImplTest.testFindGoodByName(GoodDaoImplTest.java:173)
+Actual invocation has different arguments:
+criteria.add(name=bobrik);
+-> at com.luxoftmarket.dao.impl.GoodDaoImpl.findGoodByName(GoodDaoImpl.java:49)
+
+Expected :criteria.add(name=bobrik);
+Actual   :criteria.add(name=bobrik);
+*/
+
+        verify(mockCriteria).uniqueResult();
 
         assertSame(toBeReturned, testGood);
 
