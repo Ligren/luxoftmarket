58801abb4aee2f979bd000e739017cb33da5d442
Vladyslav Kostiuchenko <sadkoua@gmail.com>
2015-05-06 12:23:57 +0400

    Add jBehave.

---
+++ src/test/java/com/luxoftmarket/AppTests.java
@@ -0,0 +1,40 @@
+package com.luxoftmarket;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTests extends TestCase {
+
+
+
+        /**
+         * Create the test case
+         *
+         * @param testName name of the test case
+         */
+        public AppTests(String testName)
+        {
+            super( testName );
+        }
+
+        /**
+         * @return the suite of tests being tested
+         */
+        public static Test suite()
+        {
+            return new TestSuite( AppTests.class );
+        }
+
+        /**
+         * Rigourous Test :-)
+         */
+        public void testApp()
+        {
+            assertTrue( true );
+        }
+
+    }
+
+
+
